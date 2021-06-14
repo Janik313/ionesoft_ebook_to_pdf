9b47cc9a7cbe06f042904edd4876c5a9815e3941
Janik 313 <janik.wyder.sek@gmail.com>
2021-06-14 11:36:45 +0200

    .

---
+++ Scripts/html_fixer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace fix
+{
+    class FixHtml
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText("C:/Temp/export.html");
+            text = text.Replace("localhost/", "localhost:7211/");
+            File.WriteAllText("C:/Temp/fixed.html", text);
+        }
+    }
+}
