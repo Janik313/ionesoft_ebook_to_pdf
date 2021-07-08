f54829a807ea4024f4047d93d6ed7f9b924ad035
Janik313 <54064741+Janik313@users.noreply.github.com>
2021-07-08 19:52:52 +0200

    .

--- WindowsFormsApp1/Forms/Form3.cs
+++ WindowsFormsApp1/Forms/Form3.cs
@@ -181,7 +181,7 @@ namespace WindowsFormsApp1
 
         private void button3_Click(object sender, EventArgs e)
         {
-            Form4 fm = new Form4();
+            Form5 fm = new Form5();
             fm.Show();
             this.Hide();
         }
