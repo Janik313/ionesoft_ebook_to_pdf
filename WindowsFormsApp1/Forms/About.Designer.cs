b169c5dce64fafc695b44df129444afb2a45220b
Janik313 <54064741+Janik313@users.noreply.github.com>
2021-07-10 01:44:05 +0200

    ui

--- WindowsFormsApp1/Forms/About.Designer.cs
+++ WindowsFormsApp1/Forms/About.Designer.cs
@@ -87,7 +87,7 @@ namespace WindowsFormsApp1.Forms
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(22, 301);
+            this.label5.Location = new System.Drawing.Point(12, 307);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(88, 25);
             this.label5.TabIndex = 4;
